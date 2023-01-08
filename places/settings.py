a8cf6e75cab7f00f7891d68476163ab4e9ac3953
Michael Akerele <michael@stacknatic.com>
2023-01-08 20:55:17 +0200

    modified settings

--- places/settings.py
+++ places/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
 import environ
+import os
 from pathlib import Path
 env = environ.Env()
 environ.Env.read_env()
@@ -27,7 +27,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, False)
+# )
+
+#DEBUG = True
+#DEBUG = env('DEBUG')
+
+DEBUG = env("DEBUG", cast=bool)
+
+
 
 ALLOWED_HOSTS = []
 
